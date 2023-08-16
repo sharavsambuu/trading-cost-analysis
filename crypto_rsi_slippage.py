bug fix in slippage calculation

crypto_rsi_slippage.py
@@ -35,7 +35,7 @@ df_
 
 
 #%%
-timeframe_by_hours  = 24
+timeframe_by_hours  = 4
 timeframe_by_minute = timeframe_by_hours*60
 
 
@@ -110,7 +110,7 @@ df['position'].value_counts()
 
 
 #%%
-plot_df = df["2020-01-12 10:00:00":"2022-03-10"].copy(deep=True)
+plot_df = df["2021-01-12":"2022-02-10"].copy(deep=True)
 
 plot_df['rsi_lower'     ] = rsi_lower
 plot_df['rsi_upper'     ] = rsi_upper
@@ -162,15 +162,12 @@ for idx, row in df[df['position']==-1].iterrows():
     if idx in df_eval.index:
         df_eval.at[idx, 'short_entry'] = float(row['Close'])
 
-
-look_ahead_shift = 1
-df_eval['long_entry' ] = df_eval['long_entry' ].shift(timeframe_by_minute+look_ahead_shift)
-df_eval['short_entry'] = df_eval['short_entry'].shift(timeframe_by_minute+look_ahead_shift)
-
+df_eval['long_entry' ] = df_eval['long_entry' ].shift(timeframe_by_minute)
+df_eval['short_entry'] = df_eval['short_entry'].shift(timeframe_by_minute)
 
 # Multiple scenarios are like how many minutes after an order executed.
 for executed_after_minute in range(1, 4+1):
-    df_eval[f"Close_{executed_after_minute}"            ] = df_eval[f"Close"].shift(executed_after_minute)
+    df_eval[f"Close_{executed_after_minute}"            ] = df_eval[f"Close"].shift(timeframe_by_minute+executed_after_minute)
     df_eval[f"long_slippage{executed_after_minute}_pct" ] = np.nan
     df_eval[f"short_slippage{executed_after_minute}_pct"] = np.nan
 
@@ -197,7 +194,7 @@ for executed_after_minute in range(1, 4+1):
 
 _, axs = plt.subplots(2, 2, figsize=(18, 8))
 
-range_by_bps = (-2.0, 2.0)
+range_by_bps = (-5.0, 5.0)
 
 l11 = axs[0,0].hist(df_eval[df_eval[f"long_slippage1_bps" ].notnull()][f"long_slippage1_bps" ].values, bins=300, range=range_by_bps, label="Long slippage BPS" , color='g')
 l12 = axs[0,0].hist(df_eval[df_eval[f"short_slippage1_bps"].notnull()][f"short_slippage1_bps"].values, bins=300, range=range_by_bps, label="Short slippage BPS", color='r')
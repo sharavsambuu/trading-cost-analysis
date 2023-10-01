2706b1823e4009538e78bc6ca3be62439c3156cb
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2023-10-02 05:33:47 +0800

    Trading Costs involved backtesting in pandas.

---
+++ btc_macross_backtesting.py
@@ -0,0 +1,168 @@
+#%%
+# MA Cross Backtesting on 1H BTC with following trade costs
+# - Slippage is 0.5BPS
+# - Taker fee is 0.04%
+# - Initial capital is 5000.0$
+# - Risk per trade is 5%
+# 
+
+#%%
+import warnings
+warnings.filterwarnings("ignore")
+def action_with_warnings():
+    warnings.warn("should not appear")
+with warnings.catch_warnings(record=True):
+    action_with_warnings()
+import yfinance          as yf
+import pandas            as pd
+import numpy             as np
+import quantstats        as qs
+import matplotlib.pyplot as plt
+
+
+#%%
+
+
+#%%
+df = yf.download('BTC-USD', interval='1h', period='12mo')
+df
+
+#%%
+df['Close'].plot()
+
+#%%
+
+
+#%%
+# Signal extraction
+
+df['MA50' ] = df['Close'].rolling(window=50 ).mean()
+df['MA200'] = df['Close'].rolling(window=200).mean()
+
+df['Signal'] = 0
+df.loc[((df['MA50'] > df['MA200']) & (df['MA50'].shift(1) <= df['MA200'].shift(1))), 'Signal'] =  1
+df.loc[((df['MA50'] < df['MA200']) & (df['MA50'].shift(1) >= df['MA200'].shift(1))), 'Signal'] = -1
+
+#%%
+df['Signal'].value_counts()
+
+#%%
+
+
+#%%
+plot_df = df["2022-11-01":"2023-03-01"]
+
+fig, ax1 = plt.subplots(1, figsize=(12, 8), sharex=True)
+
+ax1.plot(plot_df.index, plot_df['Close'], label='Close', color='black')
+ax1.plot(plot_df['MA200'], color='blue' )
+ax1.plot(plot_df['MA50' ], color='green')
+ax1.plot(plot_df[plot_df['Signal'] ==  1].index, plot_df[plot_df['Signal'] ==  1]['Close'], '^', markersize=10, color='green', label='Long' )
+ax1.plot(plot_df[plot_df['Signal'] == -1].index, plot_df[plot_df['Signal'] == -1]['Close'], 'v', markersize=10, color='red'  , label='Short')
+ax1.set_ylabel('Price')
+ax1.set_title('BTC-USD Signals')
+ax1.legend()
+ax1.grid()
+
+plt.tight_layout()
+plt.show()
+
+
+#%%
+
+
+#%%
+# position tracking 
+slippage_bps     = 0.5 / 10000 # 0.5BPS
+
+position         = 0
+entry_timestamp  = None
+entry_price      = 0
+exit_timestamp   = None
+exit_price       = 0
+position_history = []
+
+for index, row in df.iterrows():
+    if row['Signal'] != position:
+        # Exit position
+        if position != 0:
+            exit_timestamp = index
+            exit_price     = row['Close'] * (1 - slippage_bps) if position == 1 else row['Close'] * (1 + slippage_bps)
+            pct_change     = (exit_price - entry_price) / entry_price * 100
+            position_history.append((entry_timestamp, exit_timestamp, entry_price, exit_price, pct_change))
+        # Enter new position
+        if row['Signal'] == 1:
+            entry_timestamp = index
+            entry_price     = row['Close'] * (1 + slippage_bps)
+        elif row['Signal'] == -1:
+            entry_timestamp = index
+            entry_price     = row['Close'] * (1 - slippage_bps)
+        position = row['Signal']
+
+position_df = pd.DataFrame(position_history, columns=['Entry Time', 'Exit Time', 'Entry Price', 'Exit Price', 'Pct Change'])
+position_df['cumret'] = position_df['Pct Change'].cumsum()
+
+
+#%%
+
+
+#%%
+initial_capital    = 5000.0 # Initial capital in dollars
+commission_fee     = 0.04   # 0.04% commission fee per trade
+risk_per_trade     = 0.05   # 5% risk per trade
+
+account_balance = initial_capital
+balance_changes = []
+
+# Iterate through each trade in the position history
+for index, trade in position_df.iterrows():
+    pct_change       = trade['Pct Change']
+    amount_at_risk   = account_balance * risk_per_trade
+    dollar_change    = amount_at_risk  * pct_change
+    commission       = amount_at_risk  * (commission_fee/100.0)
+    balance_change   = dollar_change - commission
+    account_balance += balance_change
+    balance_changes.append(balance_change)
+
+position_df['Balance Change'] = balance_changes
+position_df['Account History'] = initial_capital + position_df['Balance Change'].cumsum()
+
+position_df['Entry Time'] = pd.to_datetime(position_df['Entry Time'])
+position_df = position_df.set_index('Entry Time')
+
+position_df['Account Change'] = position_df['Account History'].pct_change()
+
+#%%
+position_df
+
+#%%
+
+
+#%%
+qs.plots.snapshot(position_df['Account Change'], title='MA Cross on BTC Performance', show=True);
+
+#%%
+qs.plots.drawdown(position_df['Account Change'])
+
+# %%
+qs.plots.drawdowns_periods(position_df['Account Change'])
+
+# %%
+qs.plots.histogram(position_df['Account Change'])
+
+# %%
+qs.plots.monthly_heatmap(position_df['Account Change'])
+
+# %%
+qs.stats.sharpe(position_df['Account Change'])
+
+#%%
+
+
+#%%
+
+
+# %%
+
+
+# %%

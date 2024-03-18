ce3959a7ca22268955b6b92f5c401f274f1d8e55
sharavsambuu gunchinish <sharavsambuu@gmail.com>
2024-03-18 19:02:10 +0800

    preparing for portfolio rebalance.

---
+++ synthetic_portfolio_cost_exps.py
@@ -0,0 +1,225 @@
+#%%
+import pandas            as pd
+import numpy             as np
+import matplotlib.pyplot as plt
+from   datetime          import timedelta
+
+#%%
+
+
+#%%
+
+
+#%%
+# Preparing data for portfolio
+
+num_securities = 30
+num_periods    = 200
+start_date     = pd.to_datetime('2022-01-01')
+end_date       = pd.to_datetime('2023-12-31')
+
+trade_dates    = pd.to_datetime(np.sort(np.random.choice(pd.date_range(start=start_date, end=end_date, periods=num_periods + 1), num_periods, replace=False)))
+
+df = pd.DataFrame({'datetime'  : trade_dates})
+df['datetime' ] = pd.to_datetime(df['datetime'])
+df['datetime_'] = df['datetime']
+df = df.set_index('datetime')
+
+outliers_percentage = 20.0
+outliers_percentage = outliers_percentage/100.0 # percentage of all returns are outliers
+outliers_count      = int(num_periods*outliers_percentage)
+
+for idx in range(0, num_securities):
+    percentage_changes = np.random.uniform(-0.05, 0.05, num_periods).astype(float)
+    extreme_returns    = np.random.uniform(-0.09,  0.1, outliers_count).astype(float)
+    outliers_date      = df['datetime_'].sample(n=outliers_count).to_list()
+    df[f"pct_change_{idx}"] = percentage_changes
+    for outlier in list(zip(outliers_date, extreme_returns)):
+        outlier_dt  = outlier[0]
+        outlier_ret = outlier[1]
+        df.loc[outlier_dt, f"pct_change_{idx}"] = outlier_ret
+    df[f"ret_path_{idx}"  ] = df[f"pct_change_{idx}"].cumsum()
+
+df.drop('datetime_', axis=1, inplace=True)
+
+df
+
+
+#%%
+for idx in range(0, num_securities):
+    df[f'ret_path_{idx}'].plot()
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+
+
+#%%
+

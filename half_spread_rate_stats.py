d6d59386efa3c3f1e6bcd8e6f9c51aa87e40634a
Sharavsambuu Gunchinish <sharavsambuu@gmail.com>
2024-04-02 13:35:53 +0800

    Half spread rate stats for some instruments of AvaTrade.

--- half_spread_rate_stats.py
+++ half_spread_rate_stats.py
@@ -6,56 +6,43 @@ import pandas as pd
 
 
 #%%
-df = pd.read_csv("./data_mt5_ticks/usdjpy_2024_ticks.csv", index_col='datetime', parse_dates=True)
-df = df.dropna()
-
-df
-
-#%%
 
 
 #%%
-df['spread'] = df['ask'] - df['bid']
-df
-
-#%%
-df['spread'].hist(bins=300)
+csv_files = [f"{fname}" for fname in os.listdir("./data_mt5_ticks") if fname.endswith(".csv")]
+csv_files
 
 
 #%%
-df[df['spread']>0.15]
+index_list                   = []
+half_spread_rate_mean_list   = []
+half_spread_rate_median_list = []
+half_spread_rate_min_list    = []
+half_spread_rate_max_list    = []
 
+for fname in csv_files:
+    instrument = fname.replace("_2024_ticks.csv", "")
+    df = pd.read_csv(f"./data_mt5_ticks/{instrument}_2024_ticks.csv", index_col='datetime', parse_dates=True)
+    df = df.dropna()
 
-#%%
+    df['spread'          ] = df['ask'] - df['bid']
+    df['half_spread'     ] = df['spread'] / 2.0
+    df['mid_price'       ] = (df['ask'] + df['bid']) / 2.0
+    df['half_spread_rate' ] = df['half_spread'] / df['mid_price'] * 100
 
+    index_list                  .append(instrument)
+    half_spread_rate_mean_list  .append(df['half_spread_rate'].mean())
+    half_spread_rate_median_list.append(df['half_spread_rate'].median())
+    half_spread_rate_min_list   .append(df['half_spread_rate'].min())
+    half_spread_rate_max_list   .append(df['half_spread_rate'].max())
 
-#%%
-df['half_spread'     ] = df['spread'] / 2.0
-df['mid_price'       ] = (df['ask'] + df['bid']) / 2.0
-df['half_spread_rate'] = df['half_spread'] / df['mid_price']*100.0
+stats_df = pd.DataFrame(index=index_list)
+stats_df['half_spread_rate_mean'  ] = half_spread_rate_mean_list
+stats_df['half_spread_rate_median'] = half_spread_rate_median_list
+stats_df['half_spread_rate_min'   ] = half_spread_rate_min_list
+stats_df['half_spread_rate_max'   ] = half_spread_rate_max_list
 
-#%%
-
-
-#%%
-df
-
-#%%
-
-
-#%%
-df['half_spread_rate'].hist(bins=300)
-
-
-#%%
-
-
-#%%
-df['spread'].mean(), df['spread'].median(), df['spread'].min(), df['spread'].max()
-
-
-#%%
-df['half_spread_rate'].mean(), df['half_spread_rate'].median(), df['half_spread_rate'].min(), df['half_spread_rate'].max()
+stats_df
 
 #%%
 
